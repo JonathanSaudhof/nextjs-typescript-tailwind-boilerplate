ac0dfc5962721a888a3306a240adef4c4c61e096
Jonathan <Jonathan@jonathansaudhof.de>
2022-04-11 20:09:28 +0200

    chore: update Layout with header

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,19 +1,16 @@
-import React, { PropsWithChildren } from "react";
+import React from "react";
 
-const Layout = (props) => {
+const Layout = (props): JSX.Element => {
   return (
     <main className='min-h-screen min-w-screen bg-gray-800 text-gray-200'>
+      {/* HEADER */}
+      <div className='container mx-auto'>
+        <div className='fixed top-0 w-full py-6 bg-gray-800'>
+          <h1 className='text-2xl'>Star Wars Fan Page</h1>
+        </div>
+      </div>
       {props.children}
     </main>
   );
 };
-
-function Test(props) {
-  return (
-    <main className='min-h-screen min-w-screen bg-gray-800 text-gray-200'>
-      {props.children}
-    </main>
-  );
-}
-
 export default Layout;
