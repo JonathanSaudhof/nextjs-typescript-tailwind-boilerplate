21ce91338dd4ef9252251acd222c4bc9085c0a73
Jonathan <Jonathan@jonathansaudhof.de>
2022-04-11 20:11:31 +0200

    chore: updated index

--- src/pages/character/[characterId].tsx
+++ src/pages/character/[characterId].tsx
@@ -1,20 +1,58 @@
-import useCharacterDetails from "@src/hooks/useCharacterDetails";
+import { Loading } from "@components/Loading";
+import useCharacterDetails from "@hooks/useCharacterDetails";
+import useFilmMap from "@src/hooks/useFilmMap";
 import { useRouter } from "next/router";
 
 const CharacterDetails = () => {
   const router = useRouter();
   const characterId = router.query.characterId as string;
 
-  const { characterDetails, isLoading, isError } =
-    useCharacterDetails(characterId);
+  const {
+    characterDetails,
+    isLoading: characterDetailsIsLoading,
+    isError: characterDetailsIsError,
+  } = useCharacterDetails(+characterId);
 
-  console.log("isError", isError);
+  const {
+    filmMap,
+    isLoading: filmListIsLoading,
+    isError: filmListIsError,
+  } = useFilmMap();
 
-  if (isError) {
+  if (characterDetailsIsError || filmListIsError) {
     router.push("/404");
   }
 
-  return <></>;
+  if (characterDetailsIsLoading || filmListIsLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className='container mx-auto py-20'>
+      <h1>{characterDetails?.name}</h1>
+      <p>{characterDetails?.gender}</p>
+      <p>{characterDetails?.birthYear}</p>
+      <p>{characterDetails?.hairColor}</p>
+      {filmMap && (
+        <>
+          <p className=''>Films</p>
+          <ul className='gap-6'>
+            {characterDetails?.films.map((film) => {
+              const filmDetails = filmMap[film];
+              return (
+                <li className='py-6 flex gap-3' key={film}>
+                  <div>{filmDetails.episodeId}</div>
+                  <div>{filmDetails.title}</div>
+                  <div>{filmDetails.director}</div>
+                  <div>{filmDetails.openingCrawl}</div>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default CharacterDetails;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
-import useCharacterList from "@hooks/useCharacterList";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useSWRConfig } from "swr";
+import useCharacterList from "@hooks/useCharacterList";
 import { prefetchCharacterList } from "@hooks/useCharacterList";
+import useFilmMap from "@src/hooks/useFilmMap";
+import { Loading } from "@components/Loading";
 
 const Home = () => {
   const router = useRouter();
   const { page } = router.query;
   const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const { characterList, pages, isLoading, isError } =
-    useCharacterList(currentPage);
+  const { cache } = useSWRConfig();
+
+  const {
+    characterList,
+    pages,
+    isLoading: charactersIsLoading,
+    isError: charactersIsError,
+  } = useCharacterList(currentPage);
+
+  const {
+    filmMap,
+    isLoading: filmsIsLoading,
+    isError: filmsIsError,
+  } = useFilmMap();
 
   useEffect(() => {
     if (!page) {
@@ -20,16 +35,20 @@ const Home = () => {
     }
   }, [page, router]);
 
-  if (isLoading) {
-    return <>Loading</>;
+  // TODO: make better
+  function handleMouseEnter(page: number) {
+    const url = `https://swapi.dev/api/people/?page=${page}`;
+    if (cache.get(url) === undefined) {
+      prefetchCharacterList(page);
+    }
+  }
+
+  if (charactersIsLoading || filmsIsLoading) {
+    return <Loading />;
   }
 
   return (
     <div className='container mx-auto'>
-      {/* HEADER */}
-      <div className='fixed top-0 w-full py-6 bg-gray-800'>
-        <h1 className='text-2xl'>Star Wars Fan Page</h1>
-      </div>
       {/* LIST */}
       <ul className='flex flex-col gap-6 py-20'>
         {characterList.length > 0 &&
@@ -38,17 +57,25 @@ const Home = () => {
               key={character.url}
               className='flex flex-col shadow-md bg-gray-500 border rounded'
             >
-              <Link href='/characters/1'>
+              <Link href='/character/1'>
                 <a>{character.name}</a>
               </Link>
               <div>{character.gender}</div>
               <div>{character.url}</div>
-
-              <div>
-                {character.films.map((film) => (
-                  <p key={film}>{film}</p>
-                ))}
-              </div>
+              {filmMap && (
+                <div>
+                  {character.films
+                    .sort((a, b) => a - b)
+                    .map((filmId: number) => {
+                      const filmDetails = filmMap[filmId];
+                      return (
+                        <p
+                          key={filmDetails.title}
+                        >{`${filmDetails.episodeId}-${filmDetails.title}`}</p>
+                      );
+                    })}
+                </div>
+              )}
             </li>
           ))}
       </ul>
@@ -63,7 +90,7 @@ const Home = () => {
                   className={`p-2 ${
                     currentPage == index + 1 ? "text-white bg-gray-300" : ""
                   }`}
-                  onMouseEnter={prefetchCharacterList.bind(null, index + 1)}
+                  onMouseEnter={handleMouseEnter.bind(null, index + 1)}
                 >{`${index + 1}`}</a>
               </Link>
             </li>
