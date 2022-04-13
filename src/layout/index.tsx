4a9dc04f8fee90b199decbc6b20bedb93349a9cf
Jonathan <Jonathan@jonathansaudhof.de>
2022-04-13 11:30:02 +0200

    chore: refactored layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-
+import Link from "next/link";
 const Layout: React.FC = ({ children }) => {
   return (
-    <main className='min-h-screen min-w-screen bg-gray-800 text-gray-200'>
-      <div className='container mx-auto'>
-        <div className='fixed top-0 w-full py-6 bg-gray-800'>
-          <h1 className='text-2xl'>Star Wars Fan Page</h1>
+    <>
+      <header className='fixed w-full top-0 bg-gray-800 text-gray-200'>
+        <div className='container mx-auto w-auto py-6'>
+          <Link href='/'>
+            <a>
+              <h1 className='text-2xl'>Star Wars Fan Page</h1>
+            </a>
+          </Link>
         </div>
-      </div>
-      {children}
-    </main>
+      </header>
+
+      <main className='min-h-screen min-w-screen bg-gray-800 text-gray-200'>
+        {/* Header */}
+        {children}
+      </main>
+    </>
   );
 };
 
