9c670544b772e47dd3105220f952e35a086389f5
Jonathan <Jonathan@jonathansaudhof.de>
2022-04-13 11:18:15 +0200

    chore: Fix missing Type definition for Layout props

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Layout = (props): JSX.Element => {
+const Layout: React.FC = ({ children }) => {
   return (
     <main className='min-h-screen min-w-screen bg-gray-800 text-gray-200'>
-      {/* HEADER */}
       <div className='container mx-auto'>
         <div className='fixed top-0 w-full py-6 bg-gray-800'>
           <h1 className='text-2xl'>Star Wars Fan Page</h1>
         </div>
       </div>
-      {props.children}
+      {children}
     </main>
   );
 };
+
 export default Layout;
