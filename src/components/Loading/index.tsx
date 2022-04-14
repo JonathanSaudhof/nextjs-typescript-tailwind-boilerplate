b15bb9d1b47f452f26526de9c4025335b0e53df9
Jonathan <Jonathan@jonathansaudhof.de>
2022-04-14 18:19:53 +0200

    chore: fix loading height like parent

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css";
 
 export function Loading() {
   return (
-    <div className='flex flex-col gap-6 h-screen w-screen justify-center items-center'>
+    <div className='absolute top-0 flex flex-col gap-6 h-full w-full justify-center items-center'>
       <div className='italic'>Loading ...</div>
       <div className={styles.sword}>
         <div className={styles.griff}>
