e6d682ad58b180d45eca7b725c4657e5380a71f3
Jonathan <Jonathan@jonathansaudhof.de>
2022-04-14 10:18:14 +0200

    chore: smaller layout fixes and code cleanup

--- src/components/Table/TableCell.tsx
+++ src/components/Table/TableCell.tsx
@@ -14,7 +14,7 @@ export const TableCell: React.FC<TableCellProps> = ({
   const ComponentTag = asHead ? "th" : "td";
   return (
     <ComponentTag
-      className={`px-2 py-2 sm:px-4 sm:py-6 font-medium ${
+      className={`px-2 py-2 sm:px-4 sm:py-4 2xl:py-4 font-medium ${
         asHead ? "text-lg text-gray-50" : "text-gray-900"
       } ${className ? className : null}`}
       colSpan={colSpan}
