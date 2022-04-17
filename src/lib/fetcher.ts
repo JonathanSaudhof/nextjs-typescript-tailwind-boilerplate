1e9f0e1312594b5f82b4ed9d7438f315b5faed5d
JoSaudhof <jonathan.saudhof@adesso.de>
2022-04-17 14:54:41 +0200

    chore: updates lib

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,18 +1,31 @@
-import CustomError from "./customError";
+import CustomError from './customError';
+import { mutate } from 'swr';
 
-export default async function fetcher<T>(url: string): Promise<T> {
+/**
+ * fetcher
+ * @descriptio fetcher to use in swr
+ */
+export async function fetcher<T>(url: string): Promise<T> {
   const res = await fetch(url);
 
   if (!res.ok) {
     switch (res.status) {
       case 404: {
-        throw CustomError("Not found", 404);
+        throw CustomError('Not found', 404);
       }
       default: {
-        throw CustomError("Bad Request", 400);
+        throw CustomError('Bad Request', 400);
       }
     }
   }
 
   return res.json();
+}
+
+/**
+ * prefetcher
+ * @descriptio fetcher to use in swr to prefetch
+ */
+export function prefetcher(url: string) {
+  mutate(url, fetcher(url), { revalidate: false });
 }
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,19 +1,3 @@
-import { mutate } from "swr";
-import fetcher from "@/lib/fetcher";
-
-export function extractIdFromUrl(url: string) {
-  const urlPathname = new URL(url).pathname;
-
-  // split pathnames by "/" and filter empty items
-  const pathItems = urlPathname.split("/").filter((item) => item !== "");
-
-  // get the last item
-  const id = pathItems.slice(-1)[0];
-
-  // make sure id is a number
-  return +id;
-}
-
-export function prefetcher(url: string) {
-  mutate(url, fetcher(url), { revalidate: false });
+export function anyHelperFunction() {
+  return;
 }
